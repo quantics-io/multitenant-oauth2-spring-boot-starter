be950c803e6d3b101b66edab9ac0b0ac0fd9972d
Johannes Matt <johannes.matt@quantics.io>
2023-11-12 18:42:16 +0100

    Use new Spring Boot 3 autoconfiguration imports file

--- src/main/java/io/quantics/multitenant/oauth2/config/MultiTenantResourceServerAutoConfiguration.java
+++ src/main/java/io/quantics/multitenant/oauth2/config/MultiTenantResourceServerAutoConfiguration.java
@@ -3,7 +3,7 @@ package io.quantics.multitenant.oauth2.config;
 import com.nimbusds.jwt.proc.JWTClaimsSetAwareJWSKeySelector;
 import com.nimbusds.jwt.proc.JWTProcessor;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -20,7 +20,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * {@link EnableAutoConfiguration Auto-configuration} for multi-tenant resource server support.
+ * {@link AutoConfiguration Auto-configuration} for multi-tenant resource server support.
  */
 @Configuration(proxyBeanMethods = false)
 @AutoConfigureBefore({
